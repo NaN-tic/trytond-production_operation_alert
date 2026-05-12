431396007fd0ae58dab5ea13aeb5547e1d2a5df4
Àngel Àlvarez <angel@nan-tic.com>
2026-05-12 10:40:52 +0200

    initial commit

--- __init__.py
+++ __init__.py
@@ -3,8 +3,14 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 
+from . import alert
+
 def register():
     Pool.register(
+        alert.OperationAlert,
+        alert.Work,
+        alert.WorkCycle,
+        alert.Cron,
         module='production_operation_alert', type_='model')
     Pool.register(
         module='production_operation_alert', type_='wizard')
