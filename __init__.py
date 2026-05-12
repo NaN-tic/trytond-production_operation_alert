b02a082fd3c0c5c8f9040ab73e39e11fd8dbbbf4
Àngel Àlvarez <angel@nan-tic.com>
2026-05-12 09:23:40 +0200

    first commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# This file is part production_operation_alert module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+def register():
+    Pool.register(
+        module='production_operation_alert', type_='model')
+    Pool.register(
+        module='production_operation_alert', type_='wizard')
+    Pool.register(
+        module='production_operation_alert', type_='report')
---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part production_operation_alert module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class ProductionOperationAlertTestCase(ModuleTestCase):
+    'Test Production Operation Alert module'
+    module = 'production_operation_alert'
+
+del ModuleTestCase
